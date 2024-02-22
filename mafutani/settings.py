6cc81f30dfe9876ed8a263eb89888033654e0956
gibeongideon <kipngeno.gibeon@gmail.com>
2024-02-22 14:40:03 +0300

    add STATIC_ROOT

--- mafutani/settings.py
+++ mafutani/settings.py
@@ -167,7 +167,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles',]
+
+STATIC_URL = "/static/"
+STATIC_ROOT = (
+    BASE_DIR / "./static"
+) 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
